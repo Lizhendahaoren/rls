aa45e3356b9301d20dfa60114225241fb99d75f0
lz <936111074@qq.com>
2018-03-21 21:24:53 +0800

    提交sys

---
+++ rls-sys-common-api/src/main/java/com/rls/sys/common/api/SysCommonApiApplication.java
@@ -0,0 +1,12 @@
+package com.rls.sys.common.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysCommonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysCommonApiApplication.class, args);
+	}
+}
---
+++ rls-sys-common-service/src/main/java/com/rls/sys/common/service/SysCommonServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rls.sys.common.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysCommonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysCommonServiceApplication.class, args);
+	}
+}

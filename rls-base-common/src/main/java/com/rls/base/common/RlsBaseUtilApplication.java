8a76cf517317c32a85b69b59a99ab0375a3dc6b0
lz <936111074@qq.com>
2018-03-22 15:31:24 +0800

    添加base-基础包 包含util 和超类

---
+++ rls-base-common/src/main/java/com/rls/base/common/RlsBaseUtilApplication.java
@@ -0,0 +1,12 @@
+package com.rls.base.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlsBaseUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlsBaseUtilApplication.class, args);
+	}
+}

e0fde837dbedc21dc35f3009ce230e49b6378cc5
lz <936111074@qq.com>
2018-03-25 17:57:57 +0800

    提交包结构

---
+++ rls-base-common/src/main/java/com/rls/base/common/page/RBasePage.java
@@ -0,0 +1,11 @@
+package com.rls.base.common.page;
+
+/**
+ * @author ：lz
+ * @version ： 1.0
+ * @Title: ${file_name}
+ * @Description : ${todo}
+ * @date ：2018/3/25 17:57
+ */
+public class RBasePage {
+}

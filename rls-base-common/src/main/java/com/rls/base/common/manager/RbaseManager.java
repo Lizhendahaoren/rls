822384fadb21db4905bd65365aca54d08f15d53b
lz <936111074@qq.com>
2018-03-29 12:06:33 +0800

    base manager 定义增删改得方法

--- rls-base-common/src/main/java/com/rls/base/common/manager/RbaseManager.java
+++ rls-base-common/src/main/java/com/rls/base/common/manager/RbaseManager.java
@@ -1,5 +1,7 @@
 package com.rls.base.common.manager;
 
+import java.util.List;
+
 /**
  * @author ：lz
  * @version ： 1.0
@@ -7,5 +9,87 @@ package com.rls.base.common.manager;
  * @Description : ${todo}
  * @date ：2018/3/25 17:56
  */
-public class RbaseManager {
+public interface RbaseManager<T> {
+
+    /**
+     * 查询
+     *
+     * @param entity
+     * @return
+     */
+    T selectOne(T entity);
+
+    /**
+     * 通过Id查询
+     *
+     * @param id
+     * @return
+     */
+    T selectById(Object id);
+
+    /**
+     * 查询列表
+     *
+     * @param entity
+     * @return
+     */
+    List<T> selectList(T entity);
+
+    /**
+     * 获取所有对象
+     *
+     * @return
+     */
+    List<T> selectListAll();
+
+    /**
+     * 查询总记录数
+     *
+     * @param entity
+     * @return
+     */
+    Long selectCount(T entity);
+
+    /**
+     * 添加
+     *
+     * @param entity
+     */
+    void insert(T entity);
+
+
+    /**
+     * 插入 不插入null字段
+     *
+     * @param entity
+     */
+    void insertSelective(T entity);
+
+    /**
+     * 删除
+     *
+     * @param entity
+     */
+    void delete(T entity);
+
+    /**
+     * 根据Id删除
+     *
+     * @param id
+     */
+    void deleteById(Object id);
+
+    /**
+     * 根据id更新
+     *
+     * @param entity
+     */
+    void updateById(T entity);
+
+    /**
+     * 不update null
+     *
+     * @param entity
+     */
+    void updateSelectiveById(T entity);
 }

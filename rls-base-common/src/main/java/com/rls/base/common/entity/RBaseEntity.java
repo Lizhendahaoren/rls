32357966cc5627f8fa4d240af142f3143db9af55
lz <936111074@qq.com>
2018-04-16 12:05:21 +0800

    entity基类 @MappedSuperclass注解添加

--- rls-base-common/src/main/java/com/rls/base/common/entity/RBaseEntity.java
+++ rls-base-common/src/main/java/com/rls/base/common/entity/RBaseEntity.java
@@ -3,6 +3,7 @@ package com.rls.base.common.entity;
 import com.rls.base.common.annotation.RDescription;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -13,47 +14,50 @@ import java.time.LocalDate;
  * @Description : ${todo}
  * @date ：2018/3/24 14:41
  */
-public abstract class RBaseEntity implements Serializable {
+@MappedSuperclass
+public  abstract class RBaseEntity implements Serializable {
 
     @RDescription(value = "id", description = "主键")
-    private Long tId;
+    @NotNull(message = "主键不能为空")
+    protected Long id;
 
     @RDescription(value = "状态", description = "状态请参考常量定义")
-    private Integer status;
+    @NotNull(message = "状态不能为空")
+    protected Integer status;
 
     @RDescription(value = "版本", description = "hibernate维护")
-    private Integer version;
+    protected Integer version;
 
     @RDescription(description = "创建者")
-    private Long createUser;
+    @NotNull(message = "创建者不能为空")
+    protected Long createUser;
 
     @RDescription(description = "创建时间")
-    private LocalDate createTime;
+    @NotNull(message = "创建时间不能为空")
+    protected LocalDate createTime;
 
     @RDescription(description = "修改者")
-    private Long modifyUser;
+    protected Long modifyUser;
 
     @RDescription(description = "修改时间")
-    private LocalDate modifyTime;
+    protected LocalDate modifyTime;
 
-    @RDescription(description = "修改描述")
-    private String modifyDescription;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Basic(optional = false)
     @Column(name = "id", nullable = false, columnDefinition = "BIGINT UNSIGNED")
-    public Long gettId() {
-        return tId;
+    public Long getId() {
+        return id;
     }
 
-    public void settId(Long tId) {
-        this.tId = tId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
 
     @RDescription(value = "状态", description = "确保赋值增加默认值1:正常")
-    @Column(nullable = false, columnDefinition = "int default 1")
+    @Column(nullable = false, length = 5,columnDefinition = "int default 1")
     public Integer getStatus() {
         return status;
     }
@@ -106,16 +110,6 @@ public abstract class RBaseEntity implements Serializable {
 
     public void setModifyTime(LocalDate modifyTime) {
         this.modifyTime = modifyTime;
-    }
-
-
-    @Column(name = "modifyDescription", length = 500)
-    public String getModifyDescription() {
-        return modifyDescription;
-    }
-
-    public void setModifyDescription(String modifyDescription) {
-        this.modifyDescription = modifyDescription;
     }
 
     @RDescription(description = "PrePersist该注解数据插入前的操作")

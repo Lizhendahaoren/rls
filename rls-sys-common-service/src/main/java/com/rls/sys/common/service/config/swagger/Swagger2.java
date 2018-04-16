1caef15e6e78aa3fe89858e4f00c7cb813cf3e0a
lz <936111074@qq.com>
2018-04-16 21:14:56 +0800

    swagger2配置

---
+++ rls-sys-common-service/src/main/java/com/rls/sys/common/service/config/swagger/Swagger2.java
@@ -0,0 +1,40 @@
+package com.rls.sys.common.service.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author ：lz
+ * @version ： 1.0
+ * @Title: Swagger2 配置
+ * @Description : 构建restful风格的api
+ * @date ：2018/4/16 21:08
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.rls.common.service.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("rls-sys-service")
+                .description("rls-sys-service: api接口文档")
+                .termsOfServiceUrl("https://github.com/Lizhendahaoren/rls")
+                .version("1.0")
+                .build();
+    }
+}

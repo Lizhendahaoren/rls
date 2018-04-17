040b2c4842c943afe3c825dd7488b5867b294f64
lz <936111074@qq.com>
2018-04-17 22:17:31 +0800

    修改认证

---
+++ rls-sys-common/src/main/java/com/rls/sys/common/constant/SysConstant.java
@@ -0,0 +1,14 @@
+package com.rls.sys.common.constant;
+
+import com.rls.base.common.constant.RConstant;
+
+/**
+ * @author ：lz
+ * @version ： 1.0
+ * @Title: sys 常量
+ * @Description : ${todo}
+ * @date ：2018/4/17 21:55
+ */
+public class SysConstant extends RConstant {
+
+}
--- rls-sys-common/src/main/java/com/rls/sys/common/manager/SysResourceMng.java
+++ rls-sys-common/src/main/java/com/rls/sys/common/manager/SysResourceMng.java
@@ -2,6 +2,7 @@ package com.rls.sys.common.manager;
 
 
 import com.rls.base.common.annotation.RDescription;
+import com.rls.base.common.annotation.RImportant;
 import com.rls.base.common.dao.RBaseRepository;
 import com.rls.base.common.manager.RbaseManager;
 import com.rls.sys.common.entity.SysResource;
@@ -14,12 +15,10 @@ import java.util.List;
  * @author ：lz
  * @version ： 1.0
  * @Title: 资源dao
- * @Description : SysApp
+ * @Description : SysResource
  * @date ：2018/4/9 22:42
  */
 @Service
 public interface SysResourceMng extends RbaseManager<SysResource> {
 
-    @RDescription("根据用户名查找角色名")
-    List<String> findRoleNameByUserName(String userName);
 }
